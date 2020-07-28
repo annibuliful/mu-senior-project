d968b906c89f93150c3bf9dc8df5d7fd008431d4
jarupong pajakgo <lagmanmc@gmail.com>
2020-07-28 20:53:10 +0700

    init

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,12 +1,120 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div
+      class="w-full max-w-xs my-8 rounded bg-white pt-6 shadow p-8"
+      style="margin-top: 10vh; margin-left: auto; margin-right: auto;"
+    >
+      <div class="inline-flex justify-center w-full pb-8">
+        <h4
+          class=" text-center text-sm ml-2 mr-2 mt-1 cursor-pointer"
+          :class="[state.mode === 'register' ? 'active' : 'text-gray-600']"
+          @click="formMode('register')"
+        >
+          Register
+        </h4>
+        <p class="text-lg">|</p>
+        <p></p>
+        <h4
+          class="text-center text-sm ml-2 mr-2 mt-1 cursor-pointer"
+          :class="[state.mode === 'login' ? 'active' : 'text-gray-600']"
+          @click="formMode('login')"
+        >
+          Login
+        </h4>
+      </div>
+
+      <div class="mb-4">
+        <label
+          class="block text-gray-700 text-xs font-bold mb-2"
+          for="username"
+        >
+          Username
+        </label>
+        <input
+          v-model="state.username"
+          class="text-xs shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          id="username"
+          type="text"
+          placeholder="Username"
+          autocomplete="off"
+        />
+      </div>
+      <div class="mb-4">
+        <label
+          class="block text-gray-700 text-xs font-bold mb-2"
+          for="username"
+        >
+          Password
+        </label>
+        <input
+          type="password"
+          v-model="state.password"
+          class=" text-xs shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          id="password"
+          placeholder="Password"
+          autocomplete="off"
+        />
+      </div>
+      <div class="mb-4" v-if="state.mode === 'register'">
+        <label
+          class="block text-gray-700 text-xs font-bold mb-2"
+          for="username"
+        >
+          Repeat Password
+        </label>
+        <input
+          type="password"
+          v-model="state.repeatPassword"
+          class="text-xs shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          id="repeat-password"
+          placeholder="Repeat Password"
+          autocomplete="off"
+        />
+      </div>
+      <button
+        v-if="state.mode === 'register'"
+        @click="onRegister"
+        style="display: block; margin-left: auto; margin-right: auto;"
+        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded text-xs"
+      >
+        Register
+      </button>
+      <button
+        v-if="state.mode === 'login'"
+        @click="onLogin"
+        style="display: block; margin-left: auto; margin-right: auto;"
+        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded text-xs"
+      >
+        Login
+      </button>
+      <p v-if="state.error !== ''" class="text-red-500 text-center text-xs m-2">
+        {{ state.error }}
+      </p>
     </div>
-    <router-view />
   </div>
 </template>
+
+<script>
+import { reactive } from "vue";
+export default {
+  setup() {
+    const formMode = mode => {
+      state.mode = mode;
+    };
+
+    const state = reactive({
+      mode: "register",
+      username: "",
+      password: ""
+    });
+
+    return {
+      state,
+      formMode
+    };
+  }
+};
+</script>
 
 <style>
 #app {
@@ -15,18 +123,5 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
 }
 </style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+import "./index.css";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
