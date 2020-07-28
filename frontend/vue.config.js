1006df4dd64274696bdff6bd734e43374caeca9c
jarupong pajakgo <lagmanmc@gmail.com>
2020-07-28 20:34:37 +0700

    init

---
+++ frontend/vue.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  devServer: {
+    progress: false,
+    disableHostCheck: true
+  },
+  pwa: {
+    name: "VidChat",
+    themeColor: "#4DBA87",
+    msTileColor: "#000000",
+    appleMobileWebAppCapable: "yes",
+    appleMobileWebAppStatusBarStyle: "black"
+  }
+};
