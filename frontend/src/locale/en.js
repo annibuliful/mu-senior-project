b65f2f951aaedfff002491a1501005fd1ad25791
Dechapon Tongmak <dechapon.t@gmail.com>
2020-08-08 16:56:14 +0700

    Add mock vaccine data

--- frontend/src/locale/en.js
+++ frontend/src/locale/en.js
@@ -12,10 +12,117 @@ export default {
     name: "Fullname",
     birthDate: "BirthDate",
     disease: "Congenital disease",
-    vaccine: "Received vaccine"
+    vaccine: "Received vaccine",
   },
   button: {
     add: "add",
-    save: "save"
-  }
+    save: "save",
+  },
+  vaccineList: [
+    {
+      diseaseId: "disease001",
+      diseaseName: "Tuberculosis",
+    },
+    {
+      diseaseId: "disease002",
+      diseaseName: "Hepatitis B",
+    },
+    {
+      diseaseId: "disease003",
+      diseaseName: "Haemophilus influenzae type b: Hib",
+    },
+    {
+      diseaseId: "disease004",
+      diseaseName: "Diphtheria-Tetanus-Pertussis",
+    },
+    {
+      diseaseId: "disease005",
+      diseaseName: "Diphtheria and Tetanus toxoids combined",
+    },
+    {
+      diseaseId: "disease006",
+      diseaseName:
+        "Diphtheria,Tetanus toxoids, Pertussis,Hepatitis B and Haemophilus influenza type b \ncombined: DTwP-HBV-Hib",
+    },
+    {
+      diseaseId: "disease007",
+      diseaseName: "Poliomyelitis",
+    },
+    {
+      diseaseId: "disease008",
+      diseaseName: "Measles Mumps and Rubella",
+    },
+    {
+      diseaseId: "disease009",
+      diseaseName: "Japanese Encephalitis",
+    },
+    {
+      diseaseId: "disease010",
+      diseaseName: "Influenza vaccine",
+    },
+    {
+      diseaseId: "disease011",
+      diseaseName: "Hepatitis A",
+    },
+    {
+      diseaseId: "disease012",
+      diseaseName: "Varicella-zoster",
+    },
+    {
+      diseaseId: "disease013",
+      diseaseName: "Rabies",
+    },
+    {
+      diseaseId: "disease014",
+      diseaseName: "Streptococcus pneumoniae 23 - valent polysaccharide",
+    },
+    {
+      diseaseId: "disease015",
+      diseaseName: "Streptococcus pneumoniae 10 - valent conjugate",
+    },
+    {
+      diseaseId: "disease016",
+      diseaseName: "Rotavirus",
+    },
+    {
+      diseaseId: "disease017",
+      diseaseName: "Human Papillomavirus",
+    },
+    {
+      diseaseId: "disease018",
+      diseaseName: "Inactivated Poliomyelitis",
+    },
+    {
+      diseaseId: "disease019",
+      diseaseName: "Diphtheria -Tetanus - acellular Pertussis combined",
+    },
+    {
+      diseaseId: "disease020",
+      diseaseName: "Dengue",
+    },
+    {
+      diseaseId: "disease021",
+      diseaseName: "Neisseria meningitidis",
+    },
+    {
+      diseaseId: "disease022",
+      diseaseName: "Meningococcal Conjugate",
+    },
+    {
+      diseaseId: "disease023",
+      diseaseName: "Yellow fever",
+    },
+    {
+      diseaseId: "disease024",
+      diseaseName: "Pneumococcal",
+    },
+    {
+      diseaseId: "disease025",
+      diseaseName: "Cholera",
+    },
+    {
+      diseaseId: "disease026",
+      diseaseName: "Typhoid",
+    },
+  ],
 };
