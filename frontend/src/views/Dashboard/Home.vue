674c9733caf807c139b502ba0b6eb130b111a234
jarupong pajakgo <lagmanmc@gmail.com>
2020-08-19 09:40:38 +0700

    [Improvement] save state of authentication and update information

--- frontend/src/views/Dashboard/Home.vue
+++ frontend/src/views/Dashboard/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="sm:mb-20">
     <p class="text-2xl mb-10 border-b-2 border-blue-700" style="width: auto;">
-      {{ welcomeWord }} {{ profileName }}
+      {{ welcomeWord }} {{ profileName.fullname }}
     </p>
     <Calendar />
   </div>
