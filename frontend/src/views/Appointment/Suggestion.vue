e1275af8d3adcef63974bb6226e7c3f836e9ff6a
jarupong pajakgo <lagmanmc@gmail.com>
2020-09-09 07:21:31 +0700

    [Util] compute vaccine plan

--- frontend/src/views/Appointment/Suggestion.vue
+++ frontend/src/views/Appointment/Suggestion.vue
@@ -16,11 +16,19 @@
           <p>{{ val.appointmentDate | dateFormat }}</p>
         </div>
         <div>
-          asdsa
+          <img
+            :src="`${require('@/assets/icons/close-red.svg')}`"
+            @click="deleteAppointmentByIndex(index)"
+          />
         </div>
       </div>
     </div>
-    <button></button>
+    <button
+      class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded block mx-auto mt-8"
+      @click="save"
+    >
+      {{ buttonLabel.save }}
+    </button>
   </div>
 </template>
 <script>
@@ -30,11 +38,19 @@ export default {
   computed: {
     suggestionWord: function() {
       return this.$store.state.locale.appointmentSuggestion;
+    },
+    buttonLabel() {
+      return this.$store.state.locale.button;
+    },
+    listFamilies() {
+      return this.$store.state.listFamilies;
     }
   },
   data: function() {
     return {
-      listSuggestions: []
+      listSuggestions: [],
+      childId: "",
+      childInfo: {}
     };
   },
   filters: {
@@ -43,12 +59,48 @@ export default {
     }
   },
   mounted: function() {
-    const childId = this.$route.params.id;
+    this.$store.commit("listFamilies");
+    this.childId = Number(this.$route.params.id);
+
     service()
-      .suggestion.generate(childId)
+      .suggestion.generate(this.childId)
       .then(data => {
         this.listSuggestions = data;
       });
+  },
+  methods: {
+    deleteAppointmentByIndex: function(index) {
+      this.listSuggestions.splice(index, 1);
+    },
+    save: async function() {
+      for (let i = 0; i < this.listSuggestions.length; i++) {
+        const {
+          vaccineId,
+          vaccineNameNormal,
+          appointmentDate
+        } = this.listSuggestions[i];
+        await this.submit(vaccineId, vaccineNameNormal, appointmentDate);
+      }
+    },
+    submit: async function(vaccineId, vaccineName, appointmentDate) {
+      const { familyId, fullname } = this.$store.state.listFamilies.find(
+        el => el.familyId === this.childId
+      );
+      const data = {
+        dates: appointmentDate,
+        dot: "red",
+        key: appointmentDate.toString(),
+        customData: {
+          selectedVaccines: [vaccineName],
+          vaccineId,
+          childname: fullname,
+          childId: familyId,
+          time: "09:30"
+        }
+      };
+      await service().appointment.create(data);
+      this.$router.push({ name: "dashboard-index" });
+    }
   }
 };
 </script>
