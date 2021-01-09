6f4c1ac3df1b258e871f2a11eccc1fec804b6c03
Dechapon Tongmak <dechapon.t@gmail.com>
2021-01-09 11:14:45 +0700

    Create TopBar and LoggedInNews components

---
+++ frontend/src/components/TopBar.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div class="flex flex-row justify-end ">
+      <div
+        class="border-l-2 border-r-2 border-blue-700 p-1 mr-2 my-auto"
+        @click="gotoNews()"
+      >
+        News
+      </div>
+      <div class="mr-2  ">
+        <img src="../assets/icons/notification.svg" alt="" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    locale: function() {
+      return this.$store.state.locale;
+    },
+  },
+  methods: {
+    gotoNews() {
+      this.$router.push({ name: "dashboard-news" });
+    },
+  },
+};
+</script>
---
+++ frontend/src/views/News/LoggedinNews.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <TopBar></TopBar>
+    <p class="text-2xl mb-10 border-b-2 border-blue-700" style="width: auto;">
+      {{ locale.news }}
+    </p>
+    <div v-for="item in listNews" :key="item.newsId" class="my-4">
+      <NewsCard
+        :newsId="item.newsId"
+        :newsTitle="item.newsTitle"
+        :exampleContent="item.exampleContent"
+        :newsContent="item.newsContent"
+        :newsReference="item.newsReference"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import TopBar from "@/components/TopBar.vue";
+import NewsCard from "../../components/NewsCard.vue";
+export default {
+  components: {
+    NewsCard,
+    TopBar,
+  },
+
+  computed: {
+    listNews: function() {
+      return this.$store.state.locale.newsData;
+    },
+    locale: function() {
+      return this.$store.state.locale;
+    },
+  },
+};
+</script>
