7da618a6d0742856258cfd32ac36bf11a4041029
jarupong pajakgo <lagmanmc@gmail.com>
2021-01-30 16:15:47 +0700

    fix: test case

--- backend/test/shared/sql-builder.spec.ts
+++ backend/test/shared/sql-builder.spec.ts
@@ -23,9 +23,8 @@ describe('sql builder ', () => {
     const queryResult = new sqlBuilder(sqlBuilderData);
     const sqlQuery = queryResult.getById('test').toSQL().sql;
 
-    const listColumns = sqlBuilderData.columns
-      .map(col => `\\"${col}\\"`)
-      .join(',');
+    const listColumns = sqlBuilderData.columns.join(',');
+
     const expectedSql = `select ${listColumns} from users`;
     expect(expectedSql).toEqual(sqlQuery);
   });
