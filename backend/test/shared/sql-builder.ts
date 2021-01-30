1ba4cfaf073724e0847698ec4b67585754b78f76
jarupong pajakgo <lagmanmc@gmail.com>
2021-01-30 15:29:45 +0700

    feat: test case

--- backend/test/shared/sql-builder.ts
+++ backend/test/shared/sql-builder.ts
@@ -1,28 +1,41 @@
 import { ISqlBuilder, sqlBuilder } from 'src/shared/services/sql-builder';
 
+const columns = [
+  'userId',
+  'role',
+  'username',
+  'password',
+  'fullname',
+  'birthDate',
+  'profileImage',
+  'gender',
+  'phone',
+  'createAt',
+  'updateAt',
+];
 describe('sql builder ', () => {
   test('construct with complete contructor factor', () => {
     const sqlBuilderData: ISqlBuilder = {
       modelName: 'users',
       primaryKeyName: 'userId',
-      columns: [
-        'userId',
-        'role',
-        'username',
-        'password',
-        'fullname',
-        'birthDate',
-        'profileImage',
-        'gender',
-        'phone',
-        'createAt',
-        'updateAt',
-      ],
+      columns,
     };
     const queryResult = new sqlBuilder(sqlBuilderData);
     const sqlQuery = queryResult.getById('test').toSQL().sql;
 
     const expectedSql = `select ${sqlBuilderData.columns.join(',')} from users`;
+    expect(expectedSql).toEqual(sqlQuery);
+  });
+  test('contruct with only model name', () => {
+    const sqlBuilderData: ISqlBuilder = {
+      modelName: 'users',
+      primaryKeyName: 'userId',
+      columns,
+    };
+
+    const queryResult = new sqlBuilder(sqlBuilderData);
+    const sqlQuery = queryResult.getById('test').toSQL().sql;
+    const expectedSql = `select * from "users"`;
     expect(expectedSql).toEqual(sqlQuery);
   });
 });
