4f9305db3ef073402c189d0d84634f9a97cbe804
Dechapon Tongmak <dechapon.t@gmail.com>
2021-02-10 13:52:52 +0700

    fix status

--- frontend/src/services/offline/record/countRecordTime.js
+++ frontend/src/services/offline/record/countRecordTime.js
@@ -3,17 +3,16 @@ import db from "../db";
 export default async (childId, vaccineId) => {
   const listTimesRecord = await db
     .table("records")
-    .filter(record => record.childId === childId)
+    .filter((record) => record.childId === childId)
     .toArray();
   const listAllSelectedVaccines = listTimesRecord
-    .map(record => record.selectedVaccines)
+    .map((record) => record.selectedVaccines)
     .flat()
-    .map(record => record.id);
-
+    .map((record) => record.id);
   if (!listAllSelectedVaccines) return 0;
 
-  const countInjectTime = listAllSelectedVaccines.filter(id => id === vaccineId)
-    .length;
-
+  const countInjectTime = listAllSelectedVaccines.filter((id) => {
+    return id === vaccineId;
+  }).length;
   return countInjectTime;
 };
