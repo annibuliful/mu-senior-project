d45a5121a3fb27757aa4dfd23b20ffd9b0072fb9
Dechapon Tongmak <dechapon.t@gmail.com>
2021-03-17 20:48:52 +0700

    redirect skip pre login

--- frontend/src/views/Setting/Language.vue
+++ frontend/src/views/Setting/Language.vue
@@ -57,7 +57,7 @@ export default {
 
       this.$store.commit("changeLanguage", this.language);
       localStorage.setItem("language", this.language);
-      this.$router.push({ name: "pre-login" });
+      this.$router.push({ name: "dashboard-family" });
     }
   },
   computed: {
