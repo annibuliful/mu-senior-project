5638b561ef606407f6c26edb5d5925f1d6289487
lagmanzaza <lagmanmc@gmail.com>
2021-03-27 13:29:57 +0700

    feat: mock login

--- frontend/src/services/offline/auth/checkUserIdExist.js
+++ frontend/src/services/offline/auth/checkUserIdExist.js
@@ -1,9 +1,10 @@
 import db from "../db";
 
-export default async ({ username, password }) => {
-  if (!username || !password) return false;
+export default async (loginInfo) => {
+  if (!loginInfo?.username || !loginInfo?.password) return false;
   const result = (await db.table("users").toArray()).filter(
-    (el) => el.username === username && el.password === password
+    (el) =>
+      el.username === loginInfo?.username && el.password === loginInfo?.password
   );
 
   return result.length !== 0;
--- frontend/src/services/offline/auth/index.js
+++ frontend/src/services/offline/auth/index.js
@@ -6,5 +6,5 @@ export default {
   isUserIdExist,
   pin,
   register,
-  login,
+  login
 };
