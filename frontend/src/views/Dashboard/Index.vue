68afd4aec547c467036a1fb5eb1eab28f9ac2eab
Dechapon Tongmak <dechapon.t@gmail.com>
2021-03-28 09:03:19 +0700

    new nav bug img

--- frontend/src/views/Dashboard/Index.vue
+++ frontend/src/views/Dashboard/Index.vue
@@ -19,70 +19,84 @@
             v-if="isOpen"
           >
             <div class="min-h-screen">
-              <div class="flex flex-col h-full ">
-                <div class="mt-auto mb-auto cursor-pointer pb-2 pt-2">
-                  <div
-                    @click="onClickLink({ name: 'dashboard-home' })"
-                    class="ml-10 pb-4 "
-                    :class="[
-                      currentPath === '/dashboard/home' ? 'link-active' : ''
-                    ]"
-                  >
-                    <img class="inline w-8" src="@/assets/icons/home.svg" />
-                    <p class="inline mt-2 pl-3">{{ listItem.home }}</p>
-                  </div>
-                </div>
-                <div class=" mb-auto cursor-pointer">
-                  <div
-                    @click="onClickLink({ name: 'dashboard-vaccine' })"
-                    class="ml-10 pb-4 "
-                    :class="[
-                      currentPath === '/dashboard/vaccine' ? 'link-active' : ''
-                    ]"
-                  >
-                    <img class="inline w-8" src="@/assets/icons/vaccine.svg" />
-                    <p class="inline mt-2 pl-3">{{ listItem.vaccine }}</p>
-                  </div>
-                </div>
-                <div class="mb-auto cursor-pointer">
-                  <div
-                    @click="onClickLink({ name: 'dashboard-family' })"
-                    class="ml-10 pb-4 "
-                    :class="[
-                      currentPath === '/dashboard/family' ? 'link-active' : ''
-                    ]"
-                  >
-                    <img class="inline w-8" src="@/assets/icons/family.svg" />
-                    <p class="inline mt-2 pl-3">{{ listItem.family }}</p>
-                  </div>
-                </div>
-                <div class=" mb-auto cursor-pointer">
-                  <div
-                    @click="onClickLink({ name: 'dashboard-packager' })"
-                    class="ml-10 pb-4 "
-                    :class="[
-                      currentPath === '/dashboard/vaccinepackager'
-                        ? 'link-active'
-                        : ''
-                    ]"
-                  >
-                    <img class="inline w-8" src="@/assets/icons/packager.svg" />
-                    <p class="inline mt-2 pl-3">{{ listItem.packager }}</p>
-                  </div>
-                </div>
-                <div class=" mb-auto cursor-pointer">
-                  <div
-                    @click="onClickLink({ name: 'dashboard-setting' })"
-                    class="ml-10"
-                    :class="[
-                      currentPath === '/dashboard/setting' ? 'link-active' : ''
-                    ]"
-                  >
-                    <img class="inline w-8" src="@/assets/icons/setting.svg" />
-                    <p class="inline mt-2 pl-3">{{ listItem.setting }}</p>
-                  </div>
-                </div>
-              </div>
+             
+ <div class="flex flex-col h-full ">
+      <div class="mt-auto mb-auto cursor-pointer pb-2 pt-2">
+        <div
+          @click="onClickLink({ name: 'dashboard-home' })"
+          class=" pb-4 "
+          :class="[
+            currentPath === '/dashboard/home' ? 'link-active' : 'link-item'
+          ]"
+        >
+          <img class="inline w-8" src="@/assets/icons/home.svg" />
+          <p class="inline mt-2 pl-3">{{ listItem.home }}</p>
+        </div>
+      </div>
+      <div class=" mb-auto cursor-pointer">
+        <div
+          @click="onClickLink({ name: 'dashboard-vaccine' })"
+          class=" pb-4 "
+          :class="[
+            currentPath === '/dashboard/vaccine' ? 'link-active' : 'link-item'
+          ]"
+        >
+          <img class="inline w-8" src="@/assets/icons/vaccine.svg" />
+          <p class="inline mt-2 pl-3">{{ listItem.vaccine }}</p>
+        </div>
+      </div>
+      <div class="mb-auto cursor-pointer">
+        <div
+          @click="onClickLink({ name: 'dashboard-family' })"
+          class=" pb-4 "
+          :class="[
+            currentPath === '/dashboard/family' ? 'link-active' : 'link-item'
+          ]"
+        >
+          <img class="inline w-8" src="@/assets/icons/family.svg" />
+          <p class="inline mt-2 pl-3">{{ listItem.family }}</p>
+        </div>
+      </div>
+      <div class=" mb-auto cursor-pointer">
+        <div
+          @click="onClickLink({ name: 'dashboard-packager' })"
+          class=" pb-4 "
+          :class="[
+            currentPath === '/dashboard/vaccinepackager'
+              ? 'link-active'
+              : 'link-item'
+          ]"
+        >
+          <img class="inline w-8" src="@/assets/icons/packager.svg" />
+          <p class="inline mt-2 pl-3">{{ listItem.packager }}</p>
+        </div>
+      </div>
+
+      <div class=" mb-auto cursor-pointer">
+        <div
+          @click="onClickLink({ name: 'dashboard-backup' })"
+          class=" pb-4 "
+          :class="[
+            currentPath === '/dashboard/backup' ? 'link-active' : 'link-item'
+          ]"
+        >
+          <img class="inline w-8" src="@/assets/icons/sync-cloud.svg" />
+          <p class="inline mt-2 pl-3">{{ listItem.backup }}</p>
+        </div>
+      </div>
+      <div class=" mb-auto cursor-pointer">
+        <div
+          @click="onClickLink({ name: 'dashboard-setting' })"
+          class=""
+          :class="[
+            currentPath === '/dashboard/setting' ? 'link-active' : 'link-item'
+          ]"
+        >
+          <img class="inline w-8" src="@/assets/icons/setting.svg" />
+          <p class="inline mt-2 pl-3">{{ listItem.setting }}</p>
+        </div>
+      </div>
+    </div>
             </div>
           </div>
         </transition>
@@ -133,8 +147,28 @@ export default {
   width: 0;
 }
 .link-active {
-  border: 2px solid #2b6cb0;
-  border-radius: 20px;
-  padding: 20px 10px 20px 10px;
+  border: 2px dashed #050300;
+  border-radius: 10px;
+  padding: 20px 15px 20px 15px;
+  /* text-decoration: underline; */
+  background-color: rgb(245, 221, 127);
+  font-weight: bolder;
 }
+
+.link-item {
+  border: 2px dashed #503605;
+  border-radius: 10px;
+  padding: 20px 10px 20px 10px;
+  /* text-decoration: underline; */
+  background-color: cornsilk;
+  font-weight: bold;
+}
+
+
+.img-menu {
+  width: 250px;
+}
+
+
+
 </style>
