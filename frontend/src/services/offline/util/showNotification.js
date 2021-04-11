6ec4200e230f3a7c670d795dc48c6cd4c89d677a
lagmanzaza <lagmanmc@gmail.com>
2021-04-11 11:50:16 +0700

    fix: p-queue

--- frontend/src/services/offline/util/showNotification.js
+++ frontend/src/services/offline/util/showNotification.js
@@ -1,40 +1,40 @@
 import { format, addYears } from "date-fns";
 import { th, enUS } from "date-fns/locale";
 
-import PQueue from "p-queue";
-import delay from "delay";
+// import PQueue from "p-queue";
+// import delay from "delay";
 
-const queue = new PQueue({ concurrency: 1 });
-let count = 0;
-queue.on("active", () => {
-  console.log(
-    `Working on item #${++count}.  Size: ${queue.size}  Pending: ${
-      queue.pending
-    }`
-  );
-});
+// const queue = new PQueue({ concurrency: 1 });
+// let count = 0;
+// queue.on("active", () => {
+//   console.log(
+//     `Working on item #${++count}.  Size: ${queue.size}  Pending: ${
+//       queue.pending
+//     }`
+//   );
+// });
 
-const thaiDate = date =>
+const thaiDate = (date) =>
   format(addYears(new Date(date), 543), "dd MMM yyyy", {
-    locale: th
+    locale: th,
   });
-const englishDate = date =>
+const englishDate = (date) =>
   format(date, "dd MMM yyyy", {
-    locale: enUS
+    locale: enUS,
   });
 export default (listAppointments, language) => {
-  listAppointments.forEach(appointment => {
-    queue.add(function() {
-      const data = appointment.customData;
-      new Notification("appointment", {
-        tag: "syncAppointments",
-        body: `${data.childname},${data.selectedVaccines[0]} on ${
-          language === "th-TH"
-            ? thaiDate(appointment.dates)
-            : englishDate(appointment.dates)
-        }`
-      });
+  listAppointments.forEach((appointment) => {
+    // queue.add(function() {
+    const data = appointment.customData;
+    new Notification("appointment", {
+      tag: "syncAppointments",
+      body: `${data.childname},${data.selectedVaccines[0]} on ${
+        language === "th-TH"
+          ? thaiDate(appointment.dates)
+          : englishDate(appointment.dates)
+      }`,
     });
-    queue.add(() => delay(500));
+    // });
+    // queue.add(() => delay(500));
   });
 };
