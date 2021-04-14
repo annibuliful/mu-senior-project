b9834de9c6eef0c559e1abd67a9b718e618f54cb
lagmanzaza <lagmanmc@gmail.com>
2021-04-14 22:04:07 +0700

    fix: notification api

--- frontend/src/registerServiceWorker.js
+++ frontend/src/registerServiceWorker.js
@@ -1,16 +1,57 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable no-console */
-
+// eslint-disable-next-line no-undef
 import { register } from "register-service-worker";
-
+import { format, addYears } from "date-fns";
+import { th, enUS } from "date-fns/locale";
+import service from "./services";
 register("firebase-messaging-sw.js");
 register("background-sync.js");
+// navigator.serviceWorker.register("sw.js");
 
-navigator.serviceWorker.register("sw.js");
+const thaiDate = (date) =>
+  format(addYears(new Date(date), 543), "dd MMM yyyy", {
+    locale: th,
+  });
+const englishDate = (date) =>
+  format(date, "dd MMM yyyy", {
+    locale: enUS,
+  });
+
+const language = navigator.language;
 Notification.requestPermission(function(result) {
   if (result === "granted") {
-    navigator.serviceWorker.ready.then(function(registration) {
-      registration.showNotification("Notification with ServiceWorker");
-    });
+    service()
+      .appointment.cronCheckStatus()
+      .then(async () => {
+        const userInfo = JSON.parse(localStorage.getItem("userInfo"));
+        const data = await service().appointment.listNonDelete(
+          language,
+          userInfo.userId
+        );
+
+        const listAppointments = data.filter(
+          (event) => event.status === "vaccinating"
+        );
+        const registration = await navigator.serviceWorker.ready;
+        console.log("aaaa", listAppointments);
+
+        listAppointments.forEach((appointment) => {
+          const title = "appointment";
+          const data = appointment.customData;
+          const notificationData = {
+            tag: "syncAppointments",
+            body: `${data.childname},${data.selectedVaccines[0]} on ${
+              language === "th-TH"
+                ? thaiDate(appointment.dates)
+                : englishDate(appointment.dates)
+            }`,
+          };
+
+          new Notification("appointment", notificationData);
+          registration.showNotification(title, notificationData);
+        });
+      });
   }
 });
 
