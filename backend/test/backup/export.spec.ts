e5218f03814753de725916291425946fb2b646d5
lagmanzaza <lagmanmc@gmail.com>
2021-04-18 21:08:59 +0700

    fix: import

--- backend/test/backup/export.spec.ts
+++ backend/test/backup/export.spec.ts
@@ -4,23 +4,25 @@ import generateCode from '../../src/utils/generator/code-pattern';
 
 // userId: string;
 // filePath: string;
-describe('backend indexed', async () => {
-  const username = generateCode('xxxxxx');
-  const password = generateCode('xxxxxx');
-  const userService = new UserService();
-  const userInfo = await userService.create({
-    username,
-    password,
-    gender: 'female',
-    role: 'user',
-  });
-  const userId = userInfo.userId;
-  const revisionService = new RevisionService();
-  const revisionInfo = await revisionService.create({
-    userId,
-    filePath: 'MOCK_PATH',
-  });
+describe('export backend indexed', async () => {
+  it('export', async () => {
+    const username = generateCode('xxxxxx');
+    const password = generateCode('xxxxxx');
+    const userService = new UserService();
+    const userInfo = await userService.create({
+      username,
+      password,
+      gender: 'female',
+      role: 'user',
+    });
+    const userId = userInfo.userId;
+    const revisionService = new RevisionService();
+    const revisionInfo = await revisionService.create({
+      userId,
+      filePath: 'MOCK_PATH',
+    });
 
-  expect(revisionInfo.userId).toEqual(userId);
-  expect(revisionInfo.filePath).toEqual('MOCK_PATH');
+    expect(revisionInfo.userId).toEqual(userId);
+    expect(revisionInfo.filePath).toEqual('MOCK_PATH');
+  });
 });
--- backend/test/backup/import.spec.ts
+++ backend/test/backup/import.spec.ts
@@ -4,24 +4,26 @@ import generateCode from '../../src/utils/generator/code-pattern';
 
 // userId: string;
 // filePath: string;
-describe('backend indexed', async () => {
-  const username = generateCode('xxxxxx');
-  const password = generateCode('xxxxxx');
-  const userService = new UserService();
-  const userInfo = await userService.create({
-    username,
-    password,
-    gender: 'female',
-    role: 'user',
-  });
-  const userId = userInfo.userId;
-  const revisionService = new RevisionService();
-  await revisionService.create({
-    userId,
-    filePath: 'MOCK_PATH',
-  });
+describe('import backend indexed', async () => {
+  it('import', async () => {
+    const username = generateCode('xxxxxx');
+    const password = generateCode('xxxxxx');
+    const userService = new UserService();
+    const userInfo = await userService.create({
+      username,
+      password,
+      gender: 'female',
+      role: 'user',
+    });
+    const userId = userInfo.userId;
+    const revisionService = new RevisionService();
+    await revisionService.create({
+      userId,
+      filePath: 'MOCK_PATH',
+    });
 
-  const revisionInfo = await revisionService.getByUserId(userId);
-  expect(revisionInfo.userId).toEqual(userId);
-  expect(revisionInfo.filePath).toEqual('MOCK_PATH');
+    const revisionInfo = await revisionService.getByUserId(userId);
+    expect(revisionInfo.userId).toEqual(userId);
+    expect(revisionInfo.filePath).toEqual('MOCK_PATH');
+  });
 });
