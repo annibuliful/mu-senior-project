be928c3b7b854ee194e98e5af3685ab45882d105
lagmanzaza <lagmanmc@gmail.com>
2021-04-19 21:35:50 +0700

    lint

--- frontend/src/components/RecordForm.vue
+++ frontend/src/components/RecordForm.vue
@@ -151,43 +151,43 @@ export default {
       newBatchNumber: "",
       newHospitalName: "",
       newMedicalStaff: "",
-      newNoteMessage: "",
+      newNoteMessage: ""
     };
   },
   props: {
     recordCustomData: {
-      type: Object,
+      type: Object
     },
     isHasRecord: {
-      type: Boolean,
+      type: Boolean
     },
     doseNumber: {
-      type: Number,
+      type: Number
     },
     vaccineId: {
       type: String,
-      required: true,
+      required: true
     },
     childId: {
       type: Number,
-      required: true,
+      required: true
     },
     appointmentId: {
       type: Number,
-      required: true,
+      required: true
     },
     recordId: {
       type: Number,
-      required: true,
+      required: true
     },
     vaccineName: {
       type: String,
-      required: true,
+      required: true
     },
     receiveDate: {
       type: Date,
-      required: false,
-    },
+      required: false
+    }
   },
   mounted() {
     console.log("recordCustomData", this.recordCustomData);
@@ -207,7 +207,7 @@ export default {
           addYears(new Date(this.receiveDate), 543),
           "dd MMM yyyy",
           {
-            locale: th,
+            locale: th
           }
         );
       } else {
@@ -220,7 +220,7 @@ export default {
     },
     locale() {
       return this.$store.state.locale;
-    },
+    }
   },
   methods: {
     toggleEditForm: function() {
@@ -246,8 +246,8 @@ export default {
           title: this.locale.deleteRecCon,
           showCancelButton: true,
           confirmButtonText: this.locale.label.yes,
-          cancelButtonText: this.locale.label.no,
-        }).then((r) => {
+          cancelButtonText: this.locale.label.no
+        }).then(r => {
           if (r.value) {
             const data = {
               receivingDate: this.receivingDate,
@@ -261,15 +261,15 @@ export default {
                 medicalStaff: this.newMedicalStaff,
                 noteMessage: this.newNoteMessage,
                 doseNumber: this.doseNumber,
-                vaccineId: this.vaccineId,
-              },
+                vaccineId: this.vaccineId
+              }
             };
 
             this.$emit("on-record", this.hasRecord, data);
             this.$fire({
               title: this.locale.deleteRecSuc,
               type: "success",
-              timer: 3000,
+              timer: 3000
             });
           } else {
             this.hasRecord = true;
@@ -289,8 +289,8 @@ export default {
             medicalStaff: this.newMedicalStaff,
             noteMessage: this.newNoteMessage,
             doseNumber: this.doseNumber,
-            vaccineId: this.vaccineId,
-          },
+            vaccineId: this.vaccineId
+          }
         };
         this.isEdited = !this.isEdited;
 
@@ -309,9 +309,9 @@ export default {
           medicalStaff: this.newMedicalStaff,
           noteMessage: this.newNoteMessage,
           doseNumber: this.doseNumber,
-          vaccineId: this.vaccineId,
+          vaccineId: this.vaccineId
         },
-        recordId: this.recordId,
+        recordId: this.recordId
       };
       this.isEdited = false;
       this.$emit("on-save", data);
@@ -319,9 +319,9 @@ export default {
       this.$fire({
         title: this.locale.label.updateRecord,
         type: "success",
-        timer: 3000,
+        timer: 3000
       });
-    },
-  },
+    }
+  }
 };
 </script>
