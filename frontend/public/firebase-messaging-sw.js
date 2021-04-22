0f253c85f760a085d41bf4bdd681b70443173c0e
lagmanzaza <lagmanmc@gmail.com>
2021-04-22 22:27:42 +0700

    feat: notfication

--- frontend/public/firebase-messaging-sw.js
+++ frontend/public/firebase-messaging-sw.js
@@ -1,19 +1,24 @@
-// // eslint-disable-next-line no-undef
-// importScripts("https://www.gstatic.com/firebasejs/8.2.7/firebase-app.js");
-// // eslint-disable-next-line no-undef
-// importScripts("https://www.gstatic.com/firebasejs/8.2.7/firebase-messaging.js");
+// eslint-disable-next-line no-undef
+importScripts("https://www.gstatic.com/firebasejs/8.2.7/firebase-app.js");
+// eslint-disable-next-line no-undef
+importScripts("https://www.gstatic.com/firebasejs/8.2.7/firebase-messaging.js");
 
-// const VAPID_KEY =
-//   "BL3T2UrD72S7-BzpdvoECHPm50e6UOnG6Xlj39wQw5269a9xUBKMUQNMpAfNU3bMSXj_E3R696uAOkmh6tWFN6M";
-// var firebaseConfig = {
-//   apiKey: "AIzaSyAycYq1bXG5xNJtq75pzXgGd3ONoiJczEA",
-//   authDomain: "mu-senior.firebaseapp.com",
-//   projectId: "mu-senior",
-//   storageBucket: "mu-senior.appspot.com",
-//   messagingSenderId: "430977039067",
-//   appId: "1:430977039067:web:08a7760c600730f688eafe",
-//   measurementId: "G-FYFV5L0F2J",
-// };
+const VAPID_KEY =
+  "BL3T2UrD72S7-BzpdvoECHPm50e6UOnG6Xlj39wQw5269a9xUBKMUQNMpAfNU3bMSXj_E3R696uAOkmh6tWFN6M";
+var firebaseConfig = {
+  apiKey: "AIzaSyAycYq1bXG5xNJtq75pzXgGd3ONoiJczEA",
+  authDomain: "mu-senior.firebaseapp.com",
+  projectId: "mu-senior",
+  storageBucket: "mu-senior.appspot.com",
+  messagingSenderId: "430977039067",
+  appId: "1:430977039067:web:08a7760c600730f688eafe",
+  measurementId: "G-FYFV5L0F2J",
+};
 
-// const app = firebase.initializeApp(firebaseConfig);
-// app.messaging().getToken({ vapidKey: VAPID_KEY });
+const app = firebase.initializeApp(firebaseConfig);
+app
+  .messaging()
+  .getToken({ vapidKey: VAPID_KEY })
+  .then((data) => {
+    console.log("messging-token", data);
+  });
--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -1,8 +1,8 @@
 import firebase from "firebase/app";
-// import "firebase/firebase-messaging";
+import "firebase/firebase-messaging";
 import "firebase/firestore";
 import "firebase/storage";
-// import { VAPID_KEY } from "./constants/api";
+import { VAPID_KEY } from "./constants/api";
 
 var firebaseConfig = {
   apiKey: "AIzaSyAycYq1bXG5xNJtq75pzXgGd3ONoiJczEA",
@@ -11,14 +11,18 @@ var firebaseConfig = {
   storageBucket: "mu-senior.appspot.com",
   messagingSenderId: "430977039067",
   appId: "1:430977039067:web:08a7760c600730f688eafe",
-  measurementId: "G-FYFV5L0F2J"
+  measurementId: "G-FYFV5L0F2J",
 };
 
 firebase.initializeApp(firebaseConfig);
 
-// export const messaging = firebase.messaging();
+export const messaging = firebase.messaging();
 export const firestore = firebase.firestore();
 export const storage = firebase.storage();
+
+messaging.getToken({ vapidKey: VAPID_KEY }).then((data) => {
+  console.log("messging-token", data);
+});
 // export const pushMessage = (vapidKey = VAPID_KEY) =>
 //   firebase.messaging().getToken({ vapidKey });
 export default firebase;
