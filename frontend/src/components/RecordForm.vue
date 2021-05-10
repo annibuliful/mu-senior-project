a5de561a4ca0194036246d44dd73df4899fc28d8
lagmanzaza <lagmanmc@gmail.com>
2021-05-10 08:21:40 +0700

    lint

--- frontend/src/components/RecordForm.vue
+++ frontend/src/components/RecordForm.vue
@@ -214,52 +214,52 @@ export default {
       newNoteMessage: "",
       base64UrlEvidence: "",
       base64UrlSideEffect: "",
-      hospitalName: "",
+      hospitalName: ""
     };
   },
   watch: {
     isHasRecord: function(old) {
       this.hasRecord = old;
-    },
+    }
   },
   props: {
     recordCustomData: {
-      type: Object,
+      type: Object
     },
     isHasRecord: {
-      type: Boolean,
+      type: Boolean
     },
     doseNumber: {
-      type: Number,
+      type: Number
     },
     vaccineId: {
       type: String,
-      required: true,
+      required: true
     },
     childId: {
       type: Number,
-      required: true,
+      required: true
     },
     appointmentId: {
       type: Number,
-      required: true,
+      required: true
     },
     recordId: {
       type: Number,
-      required: true,
+      required: true
     },
     vaccineName: {
       type: String,
-      required: true,
+      required: true
     },
     receiveDate: {
       type: Date,
-      required: false,
+      required: false
     },
     suggestDate: {
       type: Date,
-      require: false,
-    },
+      require: false
+    }
   },
   mounted() {
     console.log(
@@ -290,7 +290,7 @@ export default {
             addYears(new Date(this.suggestDate), 543),
             "dd MMM yyyy",
             {
-              locale: th,
+              locale: th
             }
           );
         } else {
@@ -308,7 +308,7 @@ export default {
           addYears(new Date(this.receivingDate), 543),
           "dd MMM yyyy",
           {
-            locale: th,
+            locale: th
           }
         );
       } else {
@@ -321,7 +321,7 @@ export default {
     },
     locale() {
       return this.$store.state.locale;
-    },
+    }
   },
   methods: {
     onFileSideEffectChange(e) {
@@ -352,8 +352,8 @@ export default {
           title: this.locale.deleteRecCon,
           showCancelButton: true,
           confirmButtonText: this.locale.label.yes,
-          cancelButtonText: this.locale.label.no,
-        }).then((r) => {
+          cancelButtonText: this.locale.label.no
+        }).then(r => {
           if (r.value) {
             const data = {
               childId: this.childId,
@@ -370,15 +370,15 @@ export default {
                 doseNumber: this.doseNumber,
                 vaccineId: this.vaccineId,
                 base64UrlSideEffect: this.base64UrlSideEffect,
-                base64UrlEvidence: this.base64UrlEvidence,
-              },
+                base64UrlEvidence: this.base64UrlEvidence
+              }
             };
             this.isEdited = false;
             this.$emit("on-record", checkBoxValue, data);
             this.$fire({
               title: this.locale.deleteRecSuc,
               type: "success",
-              timer: 3000,
+              timer: 3000
             });
           } else {
             this.hasRecord = true;
@@ -401,8 +401,8 @@ export default {
             doseNumber: this.doseNumber,
             vaccineId: this.vaccineId,
             base64UrlSideEffect: this.base64UrlSideEffect,
-            base64UrlEvidence: this.base64UrlEvidence,
-          },
+            base64UrlEvidence: this.base64UrlEvidence
+          }
         };
 
         this.$emit("on-record", checkBoxValue, data);
@@ -422,9 +422,9 @@ export default {
           doseNumber: this.doseNumber,
           vaccineId: this.vaccineId,
           base64UrlSideEffect: this.base64UrlSideEffect,
-          base64UrlEvidence: this.base64UrlEvidence,
+          base64UrlEvidence: this.base64UrlEvidence
         },
-        recordId: this.recordId,
+        recordId: this.recordId
       };
       this.isEdited = false;
       this.$emit("on-save", data);
@@ -432,9 +432,9 @@ export default {
       this.$fire({
         title: this.locale.label.updateRecord,
         type: "success",
-        timer: 3000,
+        timer: 3000
       });
-    },
-  },
+    }
+  }
 };
 </script>
