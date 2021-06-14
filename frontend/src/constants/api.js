a578809bb149f0887ffad6b9f31ab4470fd51260
lagmanzaza <lagmanmc@gmail.com>
2021-06-14 08:04:19 +0700

    fix: covid api

--- frontend/src/constants/api.js
+++ frontend/src/constants/api.js
@@ -1,3 +1,4 @@
-export const COVID_API = "https://covid19.th-stat.com/api/open/today";
+export const COVID_API =
+  "https://covid19.th-stat.com/json/covid19v2/getTodayCases.json";
 export const VAPID_KEY =
   "BL3T2UrD72S7-BzpdvoECHPm50e6UOnG6Xlj39wQw5269a9xUBKMUQNMpAfNU3bMSXj_E3R696uAOkmh6tWFN6M";
